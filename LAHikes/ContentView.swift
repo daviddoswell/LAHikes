ae8617d403f0b5908888171eb2c1405a380bbd5c
daviddoswell <team@daviddoswell.com>
2024-02-03 16:07:53 -0500

    Initial Commit

---
+++ LAHikes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LAHikes
+//
+//  Created by David Doswell on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LAHikes/LAHikesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LAHikesApp.swift
+//  LAHikes
+//
+//  Created by David Doswell on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LAHikesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
